0c42b5cc7794f3c46e2287f32691b5f3f227cf75
Mike Richter <mrichter@preemptive.com>
2019-08-15 22:31:22 +0100

    Generate DebuggingOnAndroid-debug.apk #18488

--- build.bat
+++ build.bat
@@ -2,3 +2,7 @@ call ant -f java\simpleapp\build.xml
 call ant -f java\simpleappdir\build.xml
 call ant -f shelflife\basic\build.xml
 call android\TamperOnAndroid\gradlew -b android\TamperOnAndroid\build.gradle clean assembleRelease
+call android\DebuggingOnAndroid\gradlew -b android\DebuggingOnAndroid\build.gradle clean assembleDebug
+copy android\DebuggingOnAndroid\build\outputs\apk\debug\DebuggingOnAndroid-debug.apk android\
+@echo
+@echo Review instruction at http://wiki.preemptive.internal/Dev/DashO/Documentation/
