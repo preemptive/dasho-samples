fa5f13fcb1d1bfb3253fe2551ce925d76eee73fd
Matt Insko <minsko@preemptive.com>
2021-04-28 16:07:25 -0400

    Update versions

--- android/KotlinOnAndroid/build.gradle.kts
+++ android/KotlinOnAndroid/build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.1")
+        classpath("com.android.tools.build:gradle:4.1.3")
         classpath("com.preemptive.dasho:dasho-android:1.4.+")
         classpath(kotlin("gradle-plugin", "1.4.10"))
     }
